712de597bfe06cb359420550b98367a49062ae0b
Gerome Guilfoyle <geromeg@gmail.com>
2015-10-12 15:54:06 +0200

    basic widgets were added

---
+++ application/views/footer.php
@@ -0,0 +1,3 @@
+<div class="footer">&copy; <?php echo date("Y"); ?> Job-site</div>
+</body>
+</html>
---
+++ application/views/header.php
@@ -0,0 +1,14 @@
+<html>
+	<head>
+		<title>Home</title>
+		<link rel="Stylesheet" href="assets/css/default.css" />
+		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.7/angular.min.js"></script>
+	</head>
+<body>
+<div class="menu">
+	<ul>
+		<li>Home</li>
+		<li>Jobs</li>
+		<li>Login</li>
+	</ul>
+</div>
